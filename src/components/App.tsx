2d7201efcf3992f9b3a17f1167a8ea8a6427b6ad
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-03 18:40:16 +0700

    add todo container

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,15 @@
 
 const App: React.FC = () => {
+  const date: Date  = new Date();
+  const days: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const monthNow: string = months[date.getMonth()];
+  const dayNow: string = days[date.getDay()];
+  const todaysDate: number = date.getDate();
   return (
-    <>
-      <h1>10</h1>
-    </>
+    <div className="todo">
+      <div className="todo__container">{monthNow} {dayNow} {todaysDate}</div>
+    </div>
   )
 }
 
