8f52d257170691324384b855e63448fe3dfb7800
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-04 13:20:53 +0700

    add checkbox and trash-icon

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,23 @@ const App: React.FC = () => {
   const todaysDate: number = date.getDate();
   return (
     <div className="todo">
-      <div className="todo__container">{monthNow} {dayNow} {todaysDate}</div>
+      <div className="todo__container">
+        <header className="header">
+          {monthNow} {dayNow} {todaysDate}
+        </header>
+        <main className="main">
+          <div className="card">
+            <label className="checkbox">
+              <input type="checkbox" className="checkbox__input"/>
+              <div className="checkbox__checkmark"></div>
+            </label>
+            <div className="card__delete-overlay">
+              <span className="card__text">Pack lunches</span>
+              <button className="card__delete" type="button"></button>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
