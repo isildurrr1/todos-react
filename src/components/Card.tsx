c67327fba4f294b4fc302284cbf62f9dee7fa066
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-05 17:11:14 +0700

    add checkHandler

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,16 @@
 import { useState } from "react";
 import { CardProps } from "../types/types";
 const Card = ({text}: CardProps) => {
-  const[check, setCheck] = useState(false);
-  const lol = () => {
-    if (check) {
-      setCheck(false)
-    } else {
-      setCheck(true)
-    }
-  }
+  const[check, setCheck] = useState<boolean | null>(null);
+  const checkHandler = () => {check ? setCheck(false) : setCheck(true);}
   return (
     <div className="card">
       <label className="checkbox">
-        <input onClick={lol} type="checkbox" className="checkbox__input"/>
+        <input onClick={checkHandler} type="checkbox" className="checkbox__input"/>
         <div className="checkbox__checkmark"/>
       </label>
       <div className="card__delete-overlay">
-        <span className={`card__text ${check ? 'strike' : ''}`}>{text}</span>
+        <span className={`card__text ${check === null ? '' : check ? 'strike' : 'unstrike'}`}>{text}</span>
         <button className="card__delete" type="button"/>
       </div>
     </div>
