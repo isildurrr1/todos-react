9ac14b0b573c3b7adf2310af2abdb1ca818901ff
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-06-05 15:28:34 +0700

    add button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,9 +3,11 @@ import Main from "./Main";
 import { CardProps } from "../types/types";
 
 const App: React.FC = () => {
-  
+
   const cards: CardProps[] = [
     {text: "LOOOOL"},
+    {text: "prikol"},
+    {text: "prikol"},
     {text: "prikol"}
   ]
 
@@ -14,6 +16,10 @@ const App: React.FC = () => {
       <div className="todo__container">
         <Header/>
         <Main data={cards}/>
+        <button type="button" className="add">
+          <span className="span add__first-span"></span>
+          <span className="span add__second-span"></span>
+        </button>
       </div>
     </div>
   )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { CardProps } from "../types/types";
 const Card = ({text}: CardProps) => {
+  const[check, setCheck] = useState(false);
+  const lol = () => {
+    if (check) {
+      setCheck(false)
+    } else {
+      setCheck(true)
+    }
+  }
   return (
     <div className="card">
       <label className="checkbox">
-        <input type="checkbox" className="checkbox__input"/>
+        <input onClick={lol} type="checkbox" className="checkbox__input"/>
         <div className="checkbox__checkmark"/>
       </label>
       <div className="card__delete-overlay">
-        <span className="card__text">{text}</span>
+        <span className={`card__text ${check ? 'strike' : ''}`}>{text}</span>
         <button className="card__delete" type="button"/>
       </div>
     </div>
